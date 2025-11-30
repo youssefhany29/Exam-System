b239acb004efcae45b75af9243c798f95ca06a03
youssefhany29 <160793806+youssefhany29@users.noreply.github.com>
2025-12-01 01:00:40 +0300

    Refactor Admin class to extend User and add constructors

--- ExamSystem/src/model/Admin.java
+++ ExamSystem/src/model/Admin.java
@@ -1,12 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package model;
 
-/**
- *
- * @author Youssif
- */
-public class Admin {
+public class Admin extends User {
+    public Admin(){
+        super();
+    }
     
+    public Admin(String id, String f_name, String l_name ,String password) {
+        super(id, f_name, l_name, password);
+    }
+    
+    @Override
+    public String toString() {
+        return "Admin{id='" + getId() + "', First name='" + getFname() + ", Last name = " + getLname() +"'}";
+    }
 }
