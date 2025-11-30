bd682e2327804591825937292e8a297a2509a218
youssefhany29 <160793806+youssefhany29@users.noreply.github.com>
2025-12-01 00:25:58 +0300

    Implement User class with properties and methods
    Added fields and methods for user management.

--- ExamSystem/src/model/User.java
+++ ExamSystem/src/model/User.java
@@ -1,12 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package model;
 
-/**
- *
- * @author Youssif
- */
 public class User {
+    private String id;
+    private String f_name;
+    private String l_name;
+    private String password;
+    
+    public User(){
+    }
+    
+    public User(String id, String f_name, String l_name, String password){
+        this.id = id;
+        this.f_name = f_name;
+        this.l_name = l_name;
+        this.password = password;
+    }
+    
+    public String getId(){
+        return id;
+    }
+    
+    public void setId(String id){
+        this.id = id;
+    }
+    
+    public String getPassword(){
+        return password;
+    }
+    
+    public void setPassword(String password){
+        this.password = password;
+    }
+    
+    public String getFname(){
+        return f_name;
+    }
+    
+    public void setFname(String f_name){
+        this.f_name = f_name;
+    }
+    
+    public String getLname(){
+        return l_name;
+    } 
+    
+    public void setLname(String l_name){
+        this.l_name = l_name;
+    }
+    
+    @Override
+    public String toString() {
+        return "User{id='" + id + "', First name='" + f_name + ", Last name = " + l_name +"'}";
+    }
     
 }
