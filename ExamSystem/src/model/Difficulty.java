09c9640b4fb085bd21f3b9745b3286afc16917e7
youssefhany29 <160793806+youssefhany29@users.noreply.github.com>
2025-12-01 21:55:30 +0300

    Refactor Difficulty class to enum with levels

--- ExamSystem/src/model/Difficulty.java
+++ ExamSystem/src/model/Difficulty.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package model;
 
-/**
- *
- * @author Youssif
- */
-public class Difficulty {
-    
+public enum Difficulty {
+    EAST,
+    MEDIUM,
+    HARD
 }
