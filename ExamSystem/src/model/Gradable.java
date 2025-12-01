d07f64256857abe7d0ac819d6d79f95f70e17c7c
youssefhany29 <160793806+youssefhany29@users.noreply.github.com>
2025-12-01 21:55:05 +0300

    Refactor Gradable class to interface with getScore method

--- ExamSystem/src/model/Gradable.java
+++ ExamSystem/src/model/Gradable.java
@@ -1,12 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package model;
 
-/**
- *
- * @author Youssif
- */
-public class Gradable {
-    
+public interface Gradable {
+    int getScore();
 }
