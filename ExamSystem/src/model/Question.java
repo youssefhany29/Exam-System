feeb95cac26d7fb31ca73a929c77aab8b4c21af9
youssefhany29 <160793806+youssefhany29@users.noreply.github.com>
2025-12-01 21:48:56 +0300

    Refactor Question class to be abstract with properties

--- ExamSystem/src/model/Question.java
+++ ExamSystem/src/model/Question.java
@@ -1,12 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package model;
 
-/**
- *
- * @author Youssif
- */
-public class Question {
-    
+public abstract class Question{
+	private String text;
+	private int score;
+	private Difficulty difficulty;
+	
+	public Question(String text, int score, Difficulty difficulty) {
+		this.text = text;
+		this.score = score;
+		this.difficulty = difficulty;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public Difficulty getDifficulty() {
+		return difficulty
+	} 
+	
+	public void setDifficulty() {
+		this.difficulty = difficulty;
+	}
+	
+	public abstract boolean checkAnswer(String userAnswer);
+	
 }
