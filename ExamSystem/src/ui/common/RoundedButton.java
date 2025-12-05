15410bbbdaacdfe5707160efd3dd28c4082ddc9c
youssefhany29 <160793806+youssefhany29@users.noreply.github.com>
2025-12-05 18:32:31 +0300

    Enhance RoundedButton with customizable properties
    Added methods to customize button appearance.

--- ExamSystem/src/ui/common/RoundedButton.java
+++ ExamSystem/src/ui/common/RoundedButton.java
@@ -5,37 +5,53 @@ import javax.swing.JButton;
 
 public class RoundedButton extends JButton {
 
-    public RoundedButton(String text, int par) {
+    private Color bgColor = new Color(44, 44, 44);  
+    private Color textColor = Color.WHITE;         
+    private int radius = 30;       
+
+    public RoundedButton(String text, int radius) {
         super(text);
-        setContentAreaFilled(false);   // ما يلونّش الخلفية المستطيلة العادية
-        setFocusPainted(false);        // يشيل حافة الفوكس الزرقا
-        setBorderPainted(false);       // ما يرسمش البوردر العادي
+        this.radius = radius;
+
+        setContentAreaFilled(false);
+        setFocusPainted(false);
+        setBorderPainted(false);
+    }
+
+    public void setBackgroundColor(Color color) {
+        this.bgColor = color;
+        repaint();
+    }
+
+    public void setTextColor(Color color) {
+        this.textColor = color;
+        repaint();
+    }
+
+    public void setCornerRadius(int radius) {
+        this.radius = radius;
+        repaint();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
 
-        // تنعيم الحواف
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                             RenderingHints.VALUE_ANTIALIAS_ON);
 
         int width  = getWidth();
         int height = getHeight();
 
-        // لون الخلفية
-        g2.setColor(new Color(44, 44, 44));   // تقدر تغيره لأي لون
-        int arc = 30;                        // كل ما تكبره الزرار يبقى أروح
-        g2.fillRoundRect(0, 0, width - 1, height - 1, arc, arc);
+        g2.setColor(bgColor);
+        g2.fillRoundRect(0, 0, width, height, radius, radius);
 
-        // رسم النص في النص
+        g2.setColor(textColor);
         FontMetrics fm = g2.getFontMetrics();
         int textX = (width  - fm.stringWidth(getText())) / 2;
         int textY = (height + fm.getAscent() - fm.getDescent()) / 2;
-        g2.setColor(Color.WHITE);
         g2.drawString(getText(), textX, textY);
 
         g2.dispose();
     }
-
 }
